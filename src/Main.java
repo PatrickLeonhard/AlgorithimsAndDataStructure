01e0bc881352da050026fa8521bd518f3848ea1f
Patrick Leonhard <patrick.leonhard3@gmail.com>
2025-02-01 19:53:10 +0100

    2. Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            list.add(i);
+        }
+
+
+    }
+    public Integer BinarySearch(Integer[] array, int target) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
     }
 }
